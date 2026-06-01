a5e10092b17b1e6177eeb2e46f90962c95750e25
jalexw <jalexwhitman@gmail.com>
2026-06-01 13:21:12 -0300

    0.0.47 - include BrightnessThemeProvider in scaffolded client-global-providers.tsx

--- src/templates/app/client-global-providers.tsx.ts
+++ src/templates/app/client-global-providers.tsx.ts
@@ -1,18 +1,24 @@
 export function clientGlobalProvidersTemplate(): string {
   return `"use client";
 import type { PropsWithChildren } from "react";
-import { Toaster, LazyFramerMotionProvider, TooltipProvider } from "@schemavaults/ui";
+import { BrightnessThemeProvider, Toaster, LazyFramerMotionProvider, TooltipProvider } from "@schemavaults/ui";
 
 export default function ClientGlobalProviders({
   children
 }: PropsWithChildren) {
   return (
-    <LazyFramerMotionProvider>
-      <TooltipProvider>
-        {children}
-      </TooltipProvider>
-      <Toaster />
-    </LazyFramerMotionProvider>
+    <BrightnessThemeProvider
+      enableSystem
+      attribute={"class"}
+      defaultTheme="system"
+    >
+      <LazyFramerMotionProvider>
+        <TooltipProvider>
+          {children}
+        </TooltipProvider>
+        <Toaster />
+      </LazyFramerMotionProvider>
+    </BrightnessThemeProvider>
   );
 }
 `;
